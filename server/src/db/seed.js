871edf4270890ad45622e6a8f5134f0ae4f8bee2
Antshel23 <antsheldon23@icloud.com>
2024-10-11 11:33:03 +0100

    fixed seed

--- server/src/db/seed.js
+++ server/src/db/seed.js
@@ -1,67 +1,42 @@
-const { db } = require('./config')
-const { User, Attack } = require('../models')
+const { db } = require('./config');
+const { User, Attack, Card, CardAttack } = require('../models'); // Import CardAttack if needed
 
-function randInt (a, b) {
-  return a + Math.floor(Math.random() * (b - a))
+function randInt(a, b) {
+  return a + Math.floor(Math.random() * (b - a));
 }
 
-async function seed () {
-  await db.sync({ force: true })
+async function seed() {
+  await db.sync({ force: true });
 
   const users = await User.bulkCreate([
     { username: 'v1per' },
     { username: 'trinity' },
     { username: 'mr_spoon' }
-  ])
+  ]);
 
-  const deckNames = ['snake pit', 'the matrix', 'Doom Burger']
+  const deckNames = ['snake pit', 'the matrix', 'Doom Burger'];
   const decks = await Promise.all(
-    users.map((u, i) => u.createDeck({ name: deckNames[i], xp: randInt(0, 100)  }))
-  )
+    users.map((u, i) => u.createDeck({ name: deckNames[i], xp: randInt(0, 100) }))
+  );
 
   const cards = [
-    {
-      name: 'Arcturus Spellweaver',
-      mojo: 100,
-      stamina: 10,
-      imgUrl: 'http://localhost:5000/img/arcturus-spellweaver.jpg'
-    },
-    {
-      name: 'Nimue Mistral',
-      mojo: 100,
-      stamina: 10,
-      imgUrl: 'http://localhost:5000/img/nimue-mistral.jpg'
-    },
-    {
-      name: 'Theron Thunderstrike',
-      mojo: 100,
-      stamina: 10,
-      imgUrl: 'http://localhost:5000/img/theron-thunderstrike.jpg'
-    },
-    {
-      name: 'Lirien Moonshadow',
-      mojo: 100,
-      stamina: 10,
-      imgUrl: 'http://localhost:5000/img/lirien-moonshadow.jpg'
-    },
-    {
-      name: 'Alaric Flamecaller',
-      mojo: 100,
-      stamina: 10,
-      imgUrl: 'http://localhost:5000/img/alaric-flamecaller.jpg'
-    }
-  ]
+    { name: 'Arcturus Spellweaver', mojo: 100, stamina: 10, imgUrl: 'http://localhost:5000/img/arcturus-spellweaver.jpg' },
+    { name: 'Nimue Mistral', mojo: 100, stamina: 10, imgUrl: 'http://localhost:5000/img/nimue-mistral.jpg' },
+    { name: 'Theron Thunderstrike', mojo: 100, stamina: 10, imgUrl: 'http://localhost:5000/img/theron-thunderstrike.jpg' },
+    { name: 'Lirien Moonshadow', mojo: 100, stamina: 10, imgUrl: 'http://localhost:5000/img/lirien-moonshadow.jpg' },
+    { name: 'Alaric Flamecaller', mojo: 100, stamina: 10, imgUrl: 'http://localhost:5000/img/alaric-flamecaller.jpg' }
+  ];
 
-  const cardPromises = []
+  const cardPromises = [];
   for (const deck of decks) {
-    const cardCount = randInt(2, 5)
+    const cardCount = randInt(2, 5);
     for (let i = 0; i < cardCount; i++) {
-      const j = randInt(0, cards.length)
-      cardPromises.push(deck.createCard(cards[j]))
+      const j = randInt(0, cards.length);
+      cardPromises.push(deck.createCard(cards[j]));
     }
   }
-  
-  const createdCards = await Promise.all(cardPromises)
+
+  const createdCards = await Promise.all(cardPromises);
 
   // Seed Attacks
   const attacks = await Attack.bulkCreate([
@@ -81,14 +56,26 @@ async function seed () {
   const cardAttackPromises = [];
   for (const card of createdCards) {
     const attackCount = randInt(2, 4); // Assign 2-4 random attacks to each card
+    const assignedAttacks = new Set(); // Track assigned attacks to avoid duplicates
     for (let i = 0; i < attackCount; i++) {
       const j = randInt(0, attacks.length);
-      cardAttackPromises.push(card.addAttack(attacks[j]));
+      const attack = attacks[j];
+
+      // Only add the attack if it hasn't been assigned yet
+      if (!assignedAttacks.has(attack.id)) {
+        assignedAttacks.add(attack.id); // Mark this attack as assigned
+        cardAttackPromises.push(card.addAttack(attack).catch(err => {
+          if (err.name !== 'SequelizeUniqueConstraintError') {
+            console.error(`Error adding attack to card ${card.id}:`, err);
+          }
+        }));
+      }
     }
   }
-  await Promise.all(cardAttackPromises);
 
-  console.log('Database seeded')
+  await Promise.all(cardAttackPromises);
+  console.log('Database seeded');
 }
 
-seed()
+seed();
+
